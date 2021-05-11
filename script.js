ddee64b14e3b52fe0b8e1c1d92c09026d591efb3
Jack Ceballos <jackrceballos@gmail.com>
2021-05-10 21:59:04 -0400

    Got the ajax call working.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     // DOM Variables
+    const searchForm = $('#search-form');
     const searchCity = $('#search-city');
     const searchBtn = $('#search-btn');
     const cityList = $('#city-list');
@@ -10,18 +11,21 @@ $(document).ready(function() {
     const uvIndex = $('#uv-index');
     const forecast5Day = $('#forecast-5-day');
 
-    const queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=2054ebe0ce6d092ec2b8b6b1368ceeb0`;
-
-
-    searchBtn.click(function(e) {
-        console.log(e.target);
+    
+    // Form Submit Event
+    searchForm.submit(function(e) {
+        cityGet(searchCity.val());
         e.preventDefault();
     })
+    
+    function cityGet(cityName) {
+        const queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=2054ebe0ce6d092ec2b8b6b1368ceeb0`;
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).then(function(res) {
+            console.log(res);
+        })
+    }
 
-    $.ajax({
-        url: queryURL,
-        method: 'GET'
-    }).then(function(res) {
-        console.log()
-    })
 });
