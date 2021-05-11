fde0dfcc3d1f8a573873bf9df44b9c0a521e90aa
Jack Ceballos <jackrceballos@gmail.com>
2021-05-10 21:20:13 -0400

    Added the DOM variables working on the JS now

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 $(document).ready(function() {
     // DOM Variables
-    console.log('Loaded')
-    // API
-    const apiKey = "2054ebe0ce6d092ec2b8b6b1368ceeb0";
-    
-    
+    const searchCity = $('#search-city');
+    const searchBtn = $('#search-btn');
+    const cityList = $('#city-list');
+    const currentCity = $('#current-city');
+    const temperature = $('#temperature');
+    const humidity = $('#humidity');
+    const windSpeed = $('#wind-speed');
+    const uvIndex = $('#uv-index');
+    const forecast5Day = $('#forecast-5-day');
+
+    const queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=2054ebe0ce6d092ec2b8b6b1368ceeb0`;
+
+
+    searchBtn.click(function(e) {
+        console.log(e.target);
+        e.preventDefault();
+    })
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function(res) {
+        console.log()
+    })
 });
