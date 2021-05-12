8b68dbf74ef0ed71a29b4e9c91a049357015d53b
Jack Ceballos <jackrceballos@gmail.com>
2021-05-12 19:57:45 -0400

    Working on the prevoiously searched list.

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
     const temperature = $('#temperature');
     const humidity = $('#humidity');
     const windSpeed = $('#wind-speed');
-    const uvIndex = $('#uv-index');
     const forecast5Day = $('#forecast-5-day');
     
     
@@ -17,18 +16,29 @@ $(document).ready(function() {
         const searchVal = $('#search-city').val();
         cityGet(searchVal);
         console.log(searchVal);
-        
+
+        // City List and Local Storage
+        storeCity(searchVal);
         e.preventDefault();
     })
     
     function cityGet(cityName) {
         const queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=2054ebe0ce6d092ec2b8b6b1368ceeb0`;
+        const currentURL =`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=2054ebe0ce6d092ec2b8b6b1368ceeb0`
         $.ajax({
-            url: queryURL,
+            url: currentURL,
             method: 'GET'
         }).then(function(res) {
+            const list = res.list;
             console.log(res);
+            //currentCity.text(`${res.city.name} (${res.list[0].dt_txt})`)
         })
     }
 
+    // Local Storage and List Array
+    function storeCity(cityName) {
+        const liEl = $('<li class="list-group-item btn">');
+        liEl.text(cityName);
+        cityList.prepend(liEl);
+    }
 });
